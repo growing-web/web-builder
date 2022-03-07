c6aba9c49e24e92d209da03e110da19b3d51f145
vben <anncwb@126.com>
2022-03-07 11:50:35 +0800

    chore: release 0.0.1-beta.1

--- packages/shared/kit/__tests__/package.spec.ts
+++ packages/shared/kit/__tests__/package.spec.ts
@@ -1,12 +1,11 @@
 import { describe, expect, test } from 'vitest'
 import { getDeps } from '../src'
-import semver from 'semver'
 import path from 'path'
 
 describe('getDeps()', () => {
   test('correct merge.', async () => {
     const root = path.join(__dirname, './fixtures/package/get-deps/')
     const { deps } = await getDeps(root)
-    expect(deps.lodash).toEqual('1.0.0')
+    expect(deps.lodash).toEqual('latest')
   })
 })
