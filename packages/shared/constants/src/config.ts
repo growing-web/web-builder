accc5748fc11fb554ad4174d322b82cfa6170fa9
Vben <anncwb@126.com>
2022-03-11 18:09:22 +0800

    feat: auto web site (#7)
    * chore: sort pkg
    
    * perf: auth process web-site

--- packages/shared/constants/src/config.ts
+++ packages/shared/constants/src/config.ts
@@ -9,3 +9,5 @@ export const USER_CONFIG_FILES = [
 export const WEB_PROJECT_CONFIG_FILES = ['web-project.json']
 
 export const WEB_SITE_CONFIG = 'web-site.json'
+
+export const WEB_SITE_WORKSPACE = 'workspace'
